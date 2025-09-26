94b3940ea7c455c7c5e4b793fa226f7b56ba3e51
jinhyeook <rlawlsgur519@gamil.com>
2025-09-26 13:41:14 +0900

    update

---
+++ 250912.py
@@ -0,0 +1,17 @@
+#코테 연습문제 1
+'''
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+str_list = list()
+
+for _ in range(N):
+    T = input().rstrip() 
+    str_list.append(T)
+
+
+print(str_list)
+'''
